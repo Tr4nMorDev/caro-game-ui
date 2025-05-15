dd102257cc4295812716ebdbb4c7157752e2b5f4
Tr4nMorDev <tranphongzhouzhou@gmail.com>
2025-05-16 05:33:35 +0700

    ci/cd - vercel

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,13 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <div>
+        <h1>hello</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
